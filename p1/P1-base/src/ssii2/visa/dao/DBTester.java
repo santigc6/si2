6370bc5c412199c36849828b93305b7c361c64fa
Adrian Fernandez <adrian.fernandez.i1997@gmail.com>
2019-02-17 14:30:20 +0100

    Ej 2 hecho pero no funciona ver pagos por una excepcion java

--- p1/P1-base/src/ssii2/visa/dao/DBTester.java
+++ p1/P1-base/src/ssii2/visa/dao/DBTester.java
@@ -17,15 +17,15 @@ public class DBTester {
     // Para conexiones directas, requerimos: driver, cadena de conexión,
     // usuario y clave
     private static final String JDBC_DRIVER =
-           "org.apache.derby.jdbc.ClientDriver";
+           "org.postgresql.Driver";
 
     // TODO: Definir la cadena de conexion a la base de datos
     /*********************************************************/
     private static final String JDBC_CONNSTRING =
-            "jdbc:derby://10.1.1.1:1527/visa;create=true";
+            "jdbc:postgresql://10.1.1.1:5432/visa;create=true";
     /*********************************************************/
-    private static final String JDBC_USER = "APP";
-    private static final String JDBC_PASSWORD = "APP";
+    private static final String JDBC_USER = "alumnodb";
+    private static final String JDBC_PASSWORD = "****";
 
     // Para conexión por datasource, sólo necesitamos su nombre
     // TODO: Definir el nombre del datasource
